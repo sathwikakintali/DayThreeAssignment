0d8669f430b0099a6a9900c7fbe8780142af3012
Sathwika Kintali <Sathwika.Kintali@godigit.com>
2024-07-25 17:05:57 +0530

    added adress

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,3 +1,13 @@
+class Contacts{
+    String firstName = "Go";
+    String lastName = "Digit";
+    String address = "visakhapatnam";
+    String city = "hyderabad";
+    String state = "AndhraPradesh";
+    String email = "sathwika@gmail.com";
+    int pincode = 530027;
+    int number = 00000000;
+}
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
